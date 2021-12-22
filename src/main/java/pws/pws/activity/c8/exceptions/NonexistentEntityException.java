ad76bcf65526062f19e5e50a6c5846cff7238aaf
Muhammad Adrianto <isfan.diar123@gmail.com>
2021-12-23 03:09:30 +0700

    first commit

---
+++ src/main/java/pws/pws/activity/c8/exceptions/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package pws.pws.activity.c8.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/pws/pws/activity/c8/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.pws.activity.c8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
